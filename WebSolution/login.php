04a543c4eec2e644675fe4e957e2d4902b4ee6d3
Amritkaur-collab <bhat0260@flinders.edu.au>
2024-10-08 16:52:58 +1030

    Add files via upload

--- WebSolution/login.php
+++ WebSolution/login.php
@@ -1,17 +1,81 @@
+<!-- 1. rename index.php to registration.php -->
+<!-- 2. copy and rename registration.php to login.php -->
+<!-- 3. change page title -->
+<!-- 4. remove address, dob, userRole -->
+<!-- 5. define select query, check result, get row -->
+<!-- 6. echo id, username content -->
+<!-- 7. check password is correct -->
+<!-- 7.1 if correct echo password, address, dob, userRole -->
+<!-- 7.2 if not correct echo error message -->
+<!-- 8. add an else clause to result - user does not exist -->
+
+<?php
+require_once "inc/dbconn.inc.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login Page</title>
+    <link rel="stylesheet" href="./styles/loginstyle.css">
 </head>
+
 <body>
-    
-    <form action="auth/auth.php" method="POST">
-        <input type = 'text' name = 'user_id'/>
-        <input type = 'text' name = 'password'/>
-        <input type = 'submit'/>
+    <h1>Login Page</h1>
+
+    <div class="centred-form">
+        <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
+            <!--<div class="form-row">
+                <label class="form-label">Username</label><input class="form-input" type="text" name="username">
+            </div>-->
+            <div class="form-row">
+                <label class="form-label">EmployeeID</label><input class="form-input" type="number" name="employeeID">
+            </div>
+            <div class="form-row">
+                <label class="form-label">Password</label><input class="form-input" type="password" name="password">
+            </div>
+    </div>
+    <div class="centred-btn-container">
+        <input class="submit-btn" type="submit" name="submit" value="Login">
+    </div>
     </form>
-    
 </body>
+
 </html>
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    $employeeID = filter_input(INPUT_POST, "employeeID", FILTER_SANITIZE_NUMBER_INT);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+    if (empty($employeeID)) {
+        echo "<p style=\"text-align: center; margin-top: 10px;\">Please enter employeeID</p>";
+    }
+    elseif (empty($password)) {
+        echo "<p style=\"text-align: center; margin-top: 10px;\">Please enter a password</p>";
+    } else {
+        $sql = "SELECT * FROM users WHERE employeeID = '$employeeID';";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            echo "<p style=\"text-align:center; margin-top:10px;\">ID: " . $row["id"] . "<br>";
+            echo "EmployeeID: " . $row["employeeID"] . "<br>";
+            if (password_verify($password, $row["password"])) {
+                echo "<b>password is correct</b><br>";
+                //echo "Password: " . $row["password"] . "<br>";
+                echo "User role: " . $row["userRole"] . "</p>";
+            } else {
+                echo "<i style=\"color: red\">password incorrect</i></p><br>";
+            }
+        }else{
+            echo "<p style=\"color: red; text-align: center; margin-top:10px;\"><i>User does not exist</i></p><br>";
+        }
+    }
+}
+
+mysqli_close($conn);
+?>
