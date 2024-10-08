a97adb5cd2cbe860902d7571cfc64b7ddd719e68
Amritkaur-collab <bhat0260@flinders.edu.au>
2024-10-08 16:54:10 +1030

    Update login.php

--- WebSolution/login.php
+++ WebSolution/login.php
@@ -1,14 +1,3 @@
-<!-- 1. rename index.php to registration.php -->
-<!-- 2. copy and rename registration.php to login.php -->
-<!-- 3. change page title -->
-<!-- 4. remove address, dob, userRole -->
-<!-- 5. define select query, check result, get row -->
-<!-- 6. echo id, username content -->
-<!-- 7. check password is correct -->
-<!-- 7.1 if correct echo password, address, dob, userRole -->
-<!-- 7.2 if not correct echo error message -->
-<!-- 8. add an else clause to result - user does not exist -->
-
 <?php
 require_once "inc/dbconn.inc.php";
 ?>
