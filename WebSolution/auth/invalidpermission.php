be1ac8d30644a5dfd8760d49db42f66112a2de49
Gwendolyn-Watson <wats0376@flinders.edu.au>
2024-10-11 22:14:16 +1030

    CSS changes, side bar icons

---
+++ WebSolution/auth/invalidpermission.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Invalid Permission</title>
+</head>
+<body>
+    <h1>You do not have permission to access this page.</h1>
+    <?php header('Refresh: 2; ../home.php'); ?>
+</body>
+</html>
