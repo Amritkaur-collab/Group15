42b91433661aeea089e5767f750cab2f4877539a
jame0361 <jame0361@flinders.edu.au>
2024-10-16 23:50:15 +1030

    Add link from home page to admin homepage for admin users

--- WebSolution/inc/dbsidebar.inc.php
+++ WebSolution/inc/dbsidebar.inc.php
@@ -1,9 +1,18 @@
 <div id = 'dbsidebar'>
+    <style>
+        button {
+            cursor: pointer;
+        }
+    </style>
     <a href="home.php"><button id = 'dbsidebar-home'><span class = 'sidebar-button'></button></a><br/>
     <?php require_once "auth/permissioncheck.php";
-        if(requireRoleBtn(array('Administrator', 'Auditor', 'Factory Manager')))
+        if(requireRoleBtn(role: array('Administrator', 'Auditor', 'Factory Manager')))
         {
             echo "<a href='search.php'><button id = 'dbsidebar-search' class = 'sidebar-button'></button></a><br/>";
+        }
+        if(requireRoleBtn(role: array('Administrator')))
+        {
+            echo "<a href='admin-homepage.php'><button id = 'dbsidebar-admin' class = 'sidebar-button'></button></a><br/>";
         }
     ?>
 </div>
