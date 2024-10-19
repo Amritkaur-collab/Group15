5434808644c7733e61de1945543e62c471c8752b
netSkate <iann0024@flinders.edu.au>
2024-10-19 10:35:12 +1030

    Added new job table and user privelages

--- WebSolution/db.sql
+++ WebSolution/db.sql
@@ -1,3 +1,4 @@
+
 SET @@AUTOCOMMIT = 1;
 
 DROP DATABASE IF EXISTS Websolution;
@@ -5,13 +6,16 @@ CREATE DATABASE Websolution;
 
 USE Websolution;
 
-CREATE TABLE machines
+CREATE TABLE Machines
 (
     machine_name        CHAR(45) NOT NULL,
+    machine_location    CHAR(100),      -- New column, allows NULL
+    date_acquired       DATE,           -- New column, allows NULL
+    serial_number       CHAR(30),       -- New column, allows NULL
     PRIMARY KEY (machine_name)
 );
 
-CREATE TABLE machineLogs
+CREATE TABLE MachineLogs
 (
     timestamp           TIMESTAMP NOT NULL,
     machine_name        CHAR(45) NOT NULL,
@@ -23,13 +27,23 @@ CREATE TABLE machineLogs
     operational_status  CHAR(20),
     error_code          CHAR(4),
     production_count    INTEGER,
-    maintenance_log     varchar(200),
+    maintenance_log     VARCHAR(200),
     speed               DECIMAL(5,2),
     PRIMARY KEY (timestamp, machine_name),
-    FOREIGN KEY (machine_name) REFERENCES machines (machine_name) ON UPDATE CASCADE ON DELETE CASCADE
+    FOREIGN KEY (machine_name) REFERENCES Machines (machine_name) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE users
+CREATE TABLE jobs (
+    job_id INT AUTO_INCREMENT PRIMARY KEY,
+    job_name VARCHAR(255) NOT NULL,
+    job_duration INT NOT NULL,  -- Duration in minutes
+    machine_name VARCHAR(255),
+    FOREIGN KEY (machine_name) REFERENCES Machines(machine_name)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE Users
 (
     user_id  CHAR(20),
     password_hash  CHAR(60), 
@@ -39,8 +53,7 @@ CREATE TABLE users
     PRIMARY KEY (user_id)
 );
 
-
-CREATE TABLE jobs
+CREATE TABLE jobs_assign
 (
     job_id CHAR(20),
     user_id CHAR(20),
@@ -54,6 +67,5 @@ GRANT all privileges ON Websolution.machines TO dbadmin@localhost;
 GRANT all privileges ON Websolution.machineLogs TO dbadmin@localhost;
 GRANT all privileges ON Websolution.users TO dbadmin@localhost;
 GRANT all privileges ON Websolution.jobs TO dbadmin@localhost;
-
-
+GRANT all privileges ON Websolution.jobs_assign TO dbadmin@localhost;
 
