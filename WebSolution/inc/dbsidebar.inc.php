3d2814e6a127c691e86473c2d212821263c7e108
Gwendolyn-Watson <wats0376@flinders.edu.au>
2024-10-20 16:31:08 +1030

    CSS variables for consistent colour scheme

--- WebSolution/inc/dbsidebar.inc.php
+++ WebSolution/inc/dbsidebar.inc.php
@@ -1,5 +1,6 @@
 <div id = 'dbsidebar'>
-    <a href="home.php"><button id = 'dbsidebar-home'><span class = 'sidebar-button'></button></a><br/>
+    <a href="home.php"><button id = 'dbsidebar-home' class = 'sidebar-button'></button></a><br/>
+
     <?php require_once "auth/permissioncheck.php";
         if(requireRoleBtn(array('Administrator', 'Auditor', 'Factory Manager')))
         {
